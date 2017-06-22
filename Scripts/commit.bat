bd64c12a6e43ad89d5602aaadb79198cfac4f60d
Le Viet Dung <dunglv9961@gmail.com>
2017-06-22 13:38:02 +0700

    2017-06-22

--- Scripts/commit.bat
+++ Scripts/commit.bat
@@ -1,7 +1,7 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set backdate=%%c-%%a-%%b)
-e:
-cd E:\Data\lvdops
+rem e:
+rem cd E:\Data\lvdops
 git add -A
 git commit -a -m %backdate%
 git push
